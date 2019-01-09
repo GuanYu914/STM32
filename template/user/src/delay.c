c3972b7a80a7a5fa1b30ab3377d430c7e5984613
GuanYu914 <aa774281@gmail.com>
2019-01-09 18:28:28 +0800

    write some comments for SysTick_Init() & TimingDelay_Decreament()

--- template/user/src/delay.c
+++ template/user/src/delay.c
@@ -2,18 +2,30 @@
 
 static __IO uint32_t sysTickCounter = 0;
 
+/*SysTick function will produce interrupt when time is equal to the function parameter multiply 
+  the cycle of SystemCoreClock. You can't set to the value of function parameter more than 2 to
+  the 24th power because the SysTick timer is 24 bit.
+ 
+  Ex. SysTick_Config(SystemCoreClock / 1000000)
+  ps. SystemCoreClock is defined in a file named system_stm32f4xx.c. For this, SystemCoreClock is set to 168MHz
+
+      (168000000 / 1000000) * (1 / 168000000) = 10^-6(s) = 1us -> will produce a interrupt 
+ */
+
 void SysTick_Init(void)
 {
     	/*********************************
-	 *SystemFrequency/1000    -> 1ms *  
-	 *SystemFrequency/100000  ->10us *
-	 *SystemFrequency/1000000 -> 1us *
+	 *SystemCoreClock/1000    -> 1ms *  
+	 *SystemCoreClock/100000  ->10us *
+	 *SystemCoreClock/1000000 -> 1us *
 	 *********************************/
     	while(SysTick_Config(SystemCoreClock / 1000000) != 0)
 	{
 	    // SysTick interrupt now equals 1us
 	}
 }
+
+/* This function will be called from SysTick_Handler() */
 
 void TimingDelay_Decrement(void)
 {
